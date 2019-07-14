26cc7457cd611a31105f677c4d16e200bf64650e
xkong <kong.xu@husky.meu.edu>
2019-07-14 14:55:47 -0700

    updates to deploy to heroku 4

--- src/app/services/CourseNavigatorServiceClient.ts
+++ src/app/services/CourseNavigatorServiceClient.ts
@@ -2,12 +2,13 @@ import {Injectable} from '@angular/core';
 
 @Injectable()
 export class CourseNavigatorServiceClient {
+
   findAllCourses = () => {
-      return fetch('http://localhost:8080/api/course').
+      return fetch('https://obscure-plateau-23987.herokuapp.com/api/course').
       then(response => response.json());
   }
   findCourseById = (cid) => {
-    return fetch('http://localhost:8080/api/course/' + cid)
+    return fetch('https://obscure-plateau-23987.herokuapp.com/api/course' + cid)
       .then(response => response.json());
   }
 }
